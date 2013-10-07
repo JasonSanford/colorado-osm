21a70bd0495d59d86ed354290f15a0960c51c829
Jason Sanford <jason.sanford@mapmyfitness.com>
2013-10-07 00:16:48 -0600

    Initial commit

---
+++ osmexport.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -f colorado-latest.osm.pbf ]; then
+curl -O http://download.geofabrik.de/north-america/us/colorado-latest.osm.pbf
+fi
+
+[ -f bike_shops.geojson ] && rm bike_shops.geojson
+OSM_CONFIG_FILE=osmconf.ini ogr2ogr -f "GeoJSON" -where 'shop="bicycle"' bike_shops.geojson colorado-latest.osm.pbf points
+
+[ -f bike_paths.geojson ] && rm bike_paths.geojson
+OSM_CONFIG_FILE=osmconf.ini ogr2ogr -f "GeoJSON" -where 'highway="cycleway"' bike_paths.geojson colorado-latest.osm.pbf lines
+
+[ -f light_rail.geojson ] && rm light_rail.geojson
+OSM_CONFIG_FILE=osmconf.ini ogr2ogr -f "GeoJSON" -where 'railway="light_rail"' light_rail.geojson colorado-latest.osm.pbf lines
