645f8bf297ac97914bc1e026e8190710e0ce540e
Jason Sanford <jason.sanford@mapmyfitness.com>
2013-10-07 10:32:50 -0600

    Separate conf files. Add light rail stations

--- osmexport.sh
+++ osmexport.sh
@@ -5,13 +5,16 @@ curl -O http://download.geofabrik.de/north-america/us/colorado-latest.osm.pbf
 fi
 
 [ -f bike_shops.geojson ] && rm bike_shops.geojson
-OSM_CONFIG_FILE=osmconf.ini ogr2ogr -f "GeoJSON" -where 'shop="bicycle"' bike_shops.geojson colorado-latest.osm.pbf points
+OSM_CONFIG_FILE=osmconf_bike_shops.ini ogr2ogr -f "GeoJSON" -where 'shop="bicycle"' bike_shops.geojson colorado-latest.osm.pbf points
 
 [ -f bike_paths.geojson ] && rm bike_paths.geojson
-OSM_CONFIG_FILE=osmconf.ini ogr2ogr -f "GeoJSON" -where 'highway="cycleway"' bike_paths.geojson colorado-latest.osm.pbf lines
+OSM_CONFIG_FILE=osmconf_base.ini ogr2ogr -f "GeoJSON" -where 'highway="cycleway"' bike_paths.geojson colorado-latest.osm.pbf lines
 
 [ -f light_rail.geojson ] && rm light_rail.geojson
-OSM_CONFIG_FILE=osmconf.ini ogr2ogr -f "GeoJSON" -where 'railway="light_rail"' light_rail.geojson colorado-latest.osm.pbf lines
+OSM_CONFIG_FILE=osmconf_light_rail.ini ogr2ogr -f "GeoJSON" -where 'railway="light_rail"' light_rail.geojson colorado-latest.osm.pbf lines
 
 [ -f cafes.geojson ] && rm cafes.geojson
-OSM_CONFIG_FILE=osmconf.ini ogr2ogr -f "GeoJSON" -where 'amenity="cafe"' cafes.geojson colorado-latest.osm.pbf points
+OSM_CONFIG_FILE=osmconf_base.ini ogr2ogr -f "GeoJSON" -where 'amenity="cafe"' cafes.geojson colorado-latest.osm.pbf points
+
+[ -f light_rail_stations.geojson ] && rm light_rail_stations.geojson
+OSM_CONFIG_FILE=osmconf_light_rail.ini ogr2ogr -f "GeoJSON" -where 'railway="station" and operator="RTD"' light_rail_stations.geojson colorado-latest.osm.pbf points
